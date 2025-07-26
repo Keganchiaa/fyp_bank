93fb33e62b9298e7c3f72ae4f6a25d28cfcef30f
shane647 <23026774@rp.edu.sg>
2025-07-26 20:45:25 +0800

    123

--- bangbank/db.js
+++ bangbank/db.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: '021006',
-    database: 'fyp',
+    password: 'Republic_C207',
+    database: 'fyp_project',
 });
 
 module.exports = pool;
